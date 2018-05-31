c5f73e5b0a77a4771f80ef5f3e03226ad5445461
KeyHoi <phamdominik1805@gmail.com>
2018-05-31 04:35:20 +0200

    connect to heroku

--- facebook/views.py
+++ facebook/views.py
@@ -17,9 +17,6 @@ def connection_handler(request):
         mode = request.GET.get('hub.mode')
         token = request.GET.get('hub.verify_token')
         challenge = request.GET.get('hub.challenge')
-        print(mode)
-        print(token)
-        print(challenge)
 
         assert mode is not None
         assert token is not None
@@ -30,13 +27,13 @@ def connection_handler(request):
     else:
         body = request.body.decode('utf-8')
         body = json.loads(body)
-        print(body)
+        print('Request Body: \n{}',format(body))
 
         if body['object'] == 'page':
             for entry in body['entry']:
                 for webhook_event in entry['messaging']:
 
-                    print(webhook_event)
+                    print('Webhook Event: \n{}'.format(webhook_event))
 
                     source_id = webhook_event['sender']['id']
                     message = webhook_event['message']
@@ -44,13 +41,12 @@ def connection_handler(request):
                     if message is not None:
                         attachments = message['attachments']
 
-                        print(message)
-
                         if attachments is not None:
                             for att in attachments:
                                 if att['type'] == 'image':
                                     image_url = att['payload']['url']
                                     barcode = scan(image_url)
+                                    print("Scanned barcode: {}".format(barcode))
 
                                     product = Product.objects.get(barcode=barcode)
                                     assert product is not None
@@ -59,13 +55,13 @@ def connection_handler(request):
                                     assert Receipt is not None
 
                                     receipt_json = receipt.receipt
-                                    print(receipt_json)
+                                    print("Receipt: \n{}".format(receipt_json))
 
                                     msg = str(receipt_json['header']) + '\n\n'
                                     for par in receipt_json['paragraphs']:
                                         msg += par + '\n'
 
-                                    print(msg)
+                                    print("Message: {}\n".format(msg))
 
                                     url = c.FB_SEND_API_URL
                                     res = requests.post(url, json={
