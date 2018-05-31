50562759cd82ae12aac825b09926abff2bceb801
KeyHoi <phamdominik1805@gmail.com>
2018-05-31 12:41:03 +0200

    connect to heroku

--- facebook/views.py
+++ facebook/views.py
@@ -19,12 +19,14 @@ def connection_handler(request):
         token = request.GET.get('hub.verify_token')
         challenge = request.GET.get('hub.challenge')
 
-        assert mode is not None
-        assert token is not None
-
-        if mode == 'subscribe' and token == VERIFY_TOKEN:
+        if mode is None or token is None or challenge is None:
+            return HttpResponse(status=404)
+        elif mode == 'subscribe' and token == VERIFY_TOKEN:
             print("WEBHOOK VERIFIED")
             return HttpResponse(status=200, content=challenge)
+        else:
+            return HttpResponse(status=403)
+
     else:
         body = request.body.decode('utf-8')
         body = json.loads(body)
@@ -45,8 +47,8 @@ def connection_handler(request):
                             for att in attachments:
                                 if att['type'] == 'image':
                                     image_url = att['payload']['url']
-                                    # barcode = scan(image_url)
-                                    barcode = 4029764001807
+                                    barcode = scan(image_url)
+                                    # barcode = 4029764001807
                                     print("Scanned barcode: {}".format(barcode))
 
                                     try:
@@ -80,22 +82,3 @@ def connection_handler(request):
             return HttpResponse(status=200)
         else:
             return HttpResponse(status=404)
-
-
-@csrf_exempt
-def http_get_handler(request, mode, token, challenge):
-    print('{}\n{}\n{}'.format(mode, token, challenge))
-    assert mode is not None
-    assert token is not None
-
-    VERIFY_TOKEN = "3153b67bfc6c51a17fd558976844c6bf"
-
-    if mode == 'subscribe' and token == VERIFY_TOKEN:
-        print("WEBHOOK VERIFIED")
-        return HttpResponse(status=200, content=challenge)
-
-
-@csrf_exempt
-def test(request):
-    print("HELLO WORLD")
-    return HttpResponse(status=200)
