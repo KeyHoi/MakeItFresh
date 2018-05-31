f5898e6160e445420ea6245b88f846565309910e
KeyHoi <phamdominik1805@gmail.com>
2018-05-31 03:49:23 +0200

    connect to heroku

--- facebook/urls.py
+++ facebook/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^', views.connection_handler),
-    re_path(r'(?P<mode>\w+)&(?P<token>\w+)&(?P<challenge>\w+)', views.http_get_handler)
+    re_path(r'', views.connection_handler),
+    re_path('^', views.test),
 ]
--- facebook/views.py
+++ facebook/views.py
@@ -11,10 +11,6 @@ from scan_handler.scan_barcode import scan
 
 @csrf_exempt
 def connection_handler(request):
-    body = request.body.decode('utf-8')
-    body = json.loads(body)
-    print(body)
-
     if request.method == 'GET':
         VERIFY_TOKEN = "3153b67bfc6c51a17fd558976844c6bf"
 
@@ -31,59 +27,77 @@ def connection_handler(request):
         if mode == 'subscribe' and token == VERIFY_TOKEN:
             print("WEBHOOK VERIFIED")
             return HttpResponse(status=200, content=challenge)
-
-    if body['object'] == 'page':
-        for entry in body['entry']:
-            for webhook_event in entry['messaging']:
-
-                print(webhook_event)
-
-                source_id = webhook_event['sender']['id']
-                message = webhook_event['message']
-
-                if message is not None:
-                    attachments = message['attachments']
-
-                    print(message)
-
-                    if attachments is not None:
-                        for att in attachments:
-                            if att['type'] == 'image':
-                                image_url = att['payload']['url']
-                                barcode = scan(image_url)
-
-                                product = Product.objects.get(barcode=barcode)
-                                assert product is not None
-
-                                receipt = Receipt.objects.get(product=product)
-                                assert Receipt is not None
-
-                                receipt_json = receipt.receipt
-                                print(receipt_json)
-
-                                msg = str(receipt_json['header']) + '\n\n'
-                                for par in receipt_json['paragraphs']:
-                                    msg += par + '\n'
-
-                                print(msg)
-
-                                url = c.FB_SEND_API_URL
-                                res = requests.post(url, json={
-                                    "recipient": {
-                                        "id": source_id
-                                    },
-                                    "message": {
-                                        "text": message
-                                    }
-                                })
-
-                                assert res.status_code == 200
-
-        return HttpResponse(status=200)
     else:
-        return HttpResponse(status=404)
+        body = request.body.decode('utf-8')
+        body = json.loads(body)
+        print(body)
+
+        if body['object'] == 'page':
+            for entry in body['entry']:
+                for webhook_event in entry['messaging']:
+
+                    print(webhook_event)
+
+                    source_id = webhook_event['sender']['id']
+                    message = webhook_event['message']
+
+                    if message is not None:
+                        attachments = message['attachments']
+
+                        print(message)
+
+                        if attachments is not None:
+                            for att in attachments:
+                                if att['type'] == 'image':
+                                    image_url = att['payload']['url']
+                                    barcode = scan(image_url)
+
+                                    product = Product.objects.get(barcode=barcode)
+                                    assert product is not None
+
+                                    receipt = Receipt.objects.get(product=product)
+                                    assert Receipt is not None
+
+                                    receipt_json = receipt.receipt
+                                    print(receipt_json)
+
+                                    msg = str(receipt_json['header']) + '\n\n'
+                                    for par in receipt_json['paragraphs']:
+                                        msg += par + '\n'
+
+                                    print(msg)
+
+                                    url = c.FB_SEND_API_URL
+                                    res = requests.post(url, json={
+                                        "recipient": {
+                                            "id": source_id
+                                        },
+                                        "message": {
+                                            "text": message
+                                        }
+                                    })
+
+                                    assert res.status_code == 200
+
+            return HttpResponse(status=200)
+        else:
+            return HttpResponse(status=404)
 
 
 @csrf_exempt
 def http_get_handler(request, mode, token, challenge):
     print('{}\n{}\n{}'.format(mode, token, challenge))
+    assert mode is not None
+    assert token is not None
+
+    VERIFY_TOKEN = "3153b67bfc6c51a17fd558976844c6bf"
+
+    if mode == 'subscribe' and token == VERIFY_TOKEN:
+        print("WEBHOOK VERIFIED")
+        return HttpResponse(status=200, content=challenge)
+
+
+@csrf_exempt
+def test(request):
+    print("HELLO WORLD")
+    return HttpResponse(status=200)
