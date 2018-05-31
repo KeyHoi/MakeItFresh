1c18d4f17b0ae289e6116cd4cdaf3a18bc90d32e
KeyHoi <phamdominik1805@gmail.com>
2018-05-31 03:32:23 +0200

    connect to heroku

--- facebook/urls.py
+++ facebook/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     re_path(r'^', views.connection_handler),
-    #re_path(r'()&()&()', views.http_get_handler)
+    re_path(r'(?P<mode>\w+)&(?P<token>\w+)&(?P<challenge>\w+)', views.http_get_handler)
 ]
